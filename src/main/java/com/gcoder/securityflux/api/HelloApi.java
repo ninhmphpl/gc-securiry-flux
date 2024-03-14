255085654ba9be571a287dfda949fdbe2a55f86a
ninh <ninhmphpl@gmail.com>
2024-03-14 17:35:11 +0700

    commit AbstractFluxGlobalExceptionHandler

--- src/main/java/com/gcoder/securityflux/api/HelloApi.java
+++ src/main/java/com/gcoder/securityflux/api/HelloApi.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
